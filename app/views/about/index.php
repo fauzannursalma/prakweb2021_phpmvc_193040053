b8fe5f8077f04485cde5d097551d55e166db601b
Fauzan Nursalma <55134999+fauzannursalma@users.noreply.github.com>
2021-09-30 13:59:37 +0700

    Menyelesaikan video MVC #6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container mt-4">
   <h1>About Me</h1>
+  <img src="<?= BASEURL; ?>/img/fauzann.png" alt="Fauzan Nursalma" width="200" height="200" class="rounded-circle shadow">
   <p>Halo, nama saya <?= $data['nama'];?>, umur saya  <?= $data['umur']; ?> tahun. Saya adalah seorang  <?= $data['pekerjaan']; ?></p>
+</div>
