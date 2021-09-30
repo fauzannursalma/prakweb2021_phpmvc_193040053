2898b98e63c56cc7014c32c1e0fc8ba90cd1bc9e
Fauzan Nursalma <55134999+fauzannursalma@users.noreply.github.com>
2021-09-30 11:39:57 +0700

    Menyelesaikan video MVC #5 View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,10 +1,22 @@
 <?php
 
-class About {
-  public function index($nama = 'Fauzann', $pekerjaan = 'Mahasiswa'){
-    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends Controller{
+  public function index($nama = 'Fauzann', $pekerjaan = 'Mahasiswa', $umur = '19'){
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About Me';
+
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
+   
   }
   public function page(){
-    echo 'About/page';
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
+   
   }
 }
